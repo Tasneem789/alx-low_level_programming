12fb34e675ec409f1b3f0b339ecb6c188354d9cc
Tasneem789 <tasneemibrahim789@gmail.com>
2023-03-16 15:50:57 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ * File 4-puts.c
+ * Auth:TasneemIbrahim <375@holbertonschool.com>
+ */
+#include <stdio.h>
+/**
+ * main-prints"Programmin is like building a multilingual puzzle"
+ * followed by a new line.
+ *
+ * return:Always0.
+ */
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
