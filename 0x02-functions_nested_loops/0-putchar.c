4bc64149f7826d3b3bfd235521491557e5ea7263
Tasneem789 <tasneemibrahim789@gmail>
2023-03-21 16:26:03 -0700

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-=======
 /*
  * File: 0-putchar.c
  * Auth: TasneemIbrahim
  */
 
->>>>>>> 925a676d001ce30fa0ddeef161b03db4bb4416e1
 #include "main.h"
 
 /**
@@ -15,15 +12,16 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
+
